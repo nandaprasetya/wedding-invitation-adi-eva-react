017f1e24a7f3792c0e0ebd46befdeff3a22f5730
nandaprasetya <nandaprasetya712@gmail.com>
2026-08-03 17:07:04 +0800

    fix: bug nama tamu

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import AdiEvaInvitation from './pages/AdiEvaInvitation';
+
+function FallbackRedirect() {
+  const location = useLocation();
+  return <Navigate to={`/adi-eva${location.search}`} replace />;
+}
 
 export default function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<AdiEvaInvitation />} />
         <Route path="/adi-eva" element={<AdiEvaInvitation />} />
-        <Route path="*" element={<Navigate to="/adi-eva" replace />} />
+        <Route path="*" element={<FallbackRedirect />} />
       </Routes>
     </Router>
   );
